8cfb9a536e7c970cbc9831a3cd8629d3a47a9ad2
Jude Pineda <jude@judepineda.com>
2019-08-20 20:42:44 -0600

    Finalized website colours for today

--- colors.js
+++ colors.js
@@ -1,12 +1,18 @@
 module.exports = {
-  background: '#ffffff', // navbar & footer link color, accent middle light stripe
-  backgroundDark: '#f0e6f6', // accent light stripe
+  background: '#fff1db', // navbar & footer link color, accent middle light stripe
+  backgroundDark: '#e5dfd5', // accent light stripe,
+  // 701a49
+  primary: '#91443d', // project underline, hero text color, deep tuscan red #3
+  primaryLight: '#a64253', // underline
+  primaryDark: '#ff6f61', // main triangle right, footer, pantone 2019 living coral #2
 
-  primary: '#654236', // project underline, hero text color
-  primaryLight: '#47c6c1', // underline
-  primaryDark: '#143d59', // main triangle right, footer
-
-  secondary: '#FF7142', // main triangle left
-  secondaryLight: '#e49e5e', // secondary triangle right
-  secondaryDark: '#e25b2e', // section heading text color
+  secondary: '#5f4b8b', // main triangle left, pantone 2018 ultra violet #1
+  secondaryLight: '#424874', // secondary triangle left
+  secondaryDark: '#340068', // section heading text color, deep violet
 };
+
+/* 134074 Dark cerulean
+ * 13315c prussian blue - darker
+ * 8da8c4 pewter blue - lighter
+
+ */
